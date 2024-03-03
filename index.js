490f1d823374b1635506c0782f95cafee708f1a6
OHshajim <ajshajimmax@gmail.com>
2024-03-04 02:52:47 +0600

    name change

--- index.js
+++ index.js
@@ -11,44 +11,46 @@ async function PostDetails() {
 function allPosts(posts) {
     const postContainer = document.getElementById('post-container');
     postContainer.innerHTML = ""
+
     for (const post of posts) {
+        let title = post.title.replace("'","@")
         const div = document.createElement('div');
         div.innerHTML = " "
         div.innerHTML = `
         <div class="card card-side bg-[#F3F3F5] shadow-xl mb-10 p-5">
 
-           <div class="avatar online w-24 h-24" id="${post.id}">
+           <div class="avatar online lg:w-24 w-14 h-14 lg:h-24" id="${post.id}">
             <div class=" rounded-full">
               <img src="${post.image}" />
             </div>
            </div>
       
              <div class="card-body">
-                <div class="flex gap-5">
+                <div class="flex flex-col xl:flex-row gap-5">
                 <h4>#${post.category}</h4>
                 <h4>Author : ${post.author.name}</h4>
                 </div>
                 <h2 class="card-title text-xl font-bold">${post.title}</h2>
                 <p class="text-[#12132D99] inter text-base border-b-2 border-dashed pb-4">${post.description}</p>
 
-                <div class="flex  items-center justify-between mt-4">
+                <div class="flex flex-col xl:flex-row items-center justify-between mt-4 space-y-10">
 
                  <div class="flex items-center gap-5">
-                     <div class="flex items-center gap-2">
+                     <div class="flex flex-col xl:flex-row items-center gap-2">
                         <img src="./images/icon/massage.png" alt="massage">
                          <p>${post.comment_count}</P>
                      </div>
-                     <div class="flex items-center gap-2">
+                     <div class="flex flex-col xl:flex-row items-center gap-2">
                          <img src="./images/icon/seen.png" alt="view">
                          <p>${post.view_count}</P>
                      </div>
-                     <div class="flex items-center gap-2">
+                     <div class="flex flex-col xl:flex-row items-center gap-2">
                         <img src="./images/icon/watch.png" alt="time">
                         <p>${post.posted_time} min</P>
                      </div>
                 </div>
                 <div class="card-actions justify-end">
-                    <button onclick="inbox( '${post.title}', ${post.view_count})">
+                    <button onclick="inbox( '${title}', ${post.view_count})">
                        <img src="./images/icon/letter.png" alt="inbox">
                     </button>
                 </div>
@@ -57,11 +59,11 @@ function allPosts(posts) {
         </div>
         `;
 
-
         postContainer.appendChild(div);
-
+        
         isActive(post.isActive, post.id);
     }
+    Loader(false)
 };
 
 // check the author is active or not
@@ -87,7 +89,7 @@ function inbox(title, view) {
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="flex justify-between items-center gap-4 bg-white m-5 p-4 rounded-2xl ">
-    <h3 class="font-semibold text-base ">${title}</h3>
+    <h3 class="font-semibold text-base ">${title.replace("@","'")}</h3>
     <div class="flex gap-2 text-[#12132D99]">
     <img src="./images/icon/seen.png" alt="view">
     <p>${view}</p>
@@ -101,7 +103,9 @@ function inbox(title, view) {
 }
 
 // search part 
+
 search = async () => {
+    Loader(true);
     const input = document.getElementById("input-Field");
     const search = input.value;
 
@@ -110,9 +114,7 @@ search = async () => {
     const posts = data.posts;
     console.log(data.posts);
     console.log(search);
-
-    allPosts(posts);
-
+    allPosts(posts)
 }
 
 // latest post 
@@ -132,7 +134,7 @@ const latestPostDisplay = (posts) => {
          <div class="space-y-3">
           <div class="flex items-center gap-3 mt-3">
             <img src="./images/icon/vedio.png" alt="icon">
-            <p>${post.author.posted_date ? post.author.posted_date:'No publish date' }</p>
+            <p>${post.author.posted_date ? post.author.posted_date : 'No publish date'}</p>
           </div>
           <h2 class="font-bold text-lg  ">${post.title}</h2>
           <p class="text-[#12132D99]">${post.description}</p>
@@ -144,8 +146,8 @@ const latestPostDisplay = (posts) => {
               </div>
               </div>
                 <div class="space-y-1">
-                <h2 class="text-[#12132D] font-bold ">${post.author.name }</h2> 
-                <p class="text-[#12132D99] text-[14px] ">${post.author.designation ? post.author.designation:'Unknown'}</p>
+                <h2 class="text-[#12132D] font-bold ">${post.author.name}</h2> 
+                <p class="text-[#12132D99] text-[14px] ">${post.author.designation ? post.author.designation : 'Unknown'}</p>
                 </div>
           </div>
          </div>
@@ -153,6 +155,20 @@ const latestPostDisplay = (posts) => {
         `;
         postContainer.appendChild(div);
     });
+}
+
+// loading
+function Loader (value) {
+    const loading = document.getElementById('loading');
+    if(value){
+
+        loading.classList.remove('hidden')
+        loading.classList.add('flex')
+    }
+    else{
+        loading.classList.remove('flex')
+        loading.classList.add('hidden')
+    }
 }
 
 // call function
